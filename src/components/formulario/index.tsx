52a85460573e2e6d0cacd6b06ae8dc02e6ff4cba
marcos_maia <marcos.gomes@underprotection.com.br>
2022-05-12 12:45:46 -0300

    css do projeto

---
+++ src/components/formulario/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Botao from "../botao";
+import style from "./Formulario.module.scss";
+
+class Formulario extends React.Component {
+  render() {
+    return (
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
+          <label htmlFor="tarefa">
+            Adicione um novo estudo
+          </label>
+          <input 
+            type="text"
+            name="tarefa"
+            id="tarefa"
+            placeholder="O que você quer estudar?"
+            required
+          />
+        </div>
+        <div className={style.inputContainer}>
+          <label htmlFor="tempo">
+            Tempo
+          </label>
+          <input 
+            type="time"
+            name="tempo"
+            id="tempo"
+            step="1"
+            min="00:00:00"
+            max="01:30:00"
+            required
+          />
+        </div>
+        <Botao />
+      </form>
+    )
+  }
+}
+
+export default Formulario;
