17cf80ecf4bc85f260ac9e1a9b0b72e9123b3cb7
mamgjunior <mamgjunior@hotmail.com>
2023-12-10 12:01:47 -0300

    Mudando a forma que se modifica as cores

--- organo/src/App.js
+++ organo/src/App.js
@@ -5,43 +5,36 @@ import Time from './componentes/Time';
 
 function App() {
 
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'Programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      cor: '#57C278',
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      cor: '#82CFFA',
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      cor: '#A6D157',
     },
     {
       nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      cor: '#E06B69',
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      cor: '#DB6EBF',
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      cor: '#FFBA05',
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      cor: '#FF8A29',
     }
-  ]
+  ]);
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -52,7 +45,17 @@ function App() {
 
   const deletarColaborador = () => {
     console.log('clickou aqui...');
-  } 
+  }
+
+  const mudarCorDoTime = (cor, nome) => {
+    setTimes(
+      times.map(element => {
+        if (element.nome === nome)
+          element.cor = cor;
+        return element;
+      })
+    );
+  }
 
   return (
     <div
@@ -72,6 +75,7 @@ function App() {
             time={time}
             colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
             aoDeletar={deletarColaborador}
+            mudarCor={mudarCorDoTime}
           />
         ))
       }
--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -1,23 +1,38 @@
-import Colaborador from '../Colaborador'
+import Colaborador from '../Colaborador';
+import hexToRgba from 'hex-to-rgba';
+
 import './Time.css'
 
-const Time = ({ time, colaboradores, aoDeletar }) => {
-  const css = { backgroundColor: time.corSecundaria }
+const Time = ({
+  time,
+  colaboradores,
+  aoDeletar,
+  mudarCor
+}) => {
+  const css = { backgroundColor: hexToRgba(time.cor, '0.6') }
 
   return (
     colaboradores.length > 0 &&
-    <section 
-      className='time' 
+    <section
+      className='time'
       style={css}
     >
+      <input
+        value={time.cor}
+        type='color'
+        className='input-cor'
+        onChange={event => mudarCor(event.target.value, time.nome)}
+      />
+
       <h3 style={{ borderColor: time.corPrimaria }}>{time.nome}</h3>
+
       <div className='colaboradores'>
         {
           colaboradores.map((colaborador, indice) => {
             return (
               < Colaborador
                 key={indice}
-                corDeFundo={time.corPrimaria}
+                corDeFundo={time.cor}
                 colaborador={colaborador}
                 aoDeletar={aoDeletar}
               />
