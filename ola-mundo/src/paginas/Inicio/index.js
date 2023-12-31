8a79260d3bf24e5847ba26db4d43969921e854b2
mamgjunior <mamgjunior@hotmail.com>
2023-12-31 10:00:03 -0300

    Criando rotas aninhadas

--- ola-mundo/src/paginas/Inicio/index.js
+++ ola-mundo/src/paginas/Inicio/index.js
@@ -1,23 +1,18 @@
-import Banner from "components/Banner";
 import posts from 'json/posts.json';
 import styles from './Inicio.module.css';
 import Post from "components/Post";
 
 export default function Inicio() {
   return (
-    <main>
-      <Banner />
-
-      <ul className={styles.posts}>
-        {
-          posts.map(post => (
-            <Post
-              key={post.id}
-              post={post}
-            />
-          ))
-        }
-      </ul>
-    </main>
+    <ul className={styles.posts}>
+      {
+        posts.map(post => (
+          <Post
+            key={post.id}
+            post={post}
+          />
+        ))
+      }
+    </ul>
   )
 }
--- ola-mundo/src/routes.js
+++ ola-mundo/src/routes.js
@@ -4,6 +4,7 @@ import Menu from "./components/Menu";
 import Inicio from "./paginas/Inicio";
 import Sobremim from "./paginas/SobreMim";
 import Rodape from "components/Rodape";
+import PaginaPadrao from "components/PaginaPadrao";
 
 function AppRoutes() {
   return (
@@ -11,8 +12,11 @@ function AppRoutes() {
       <Menu />
 
       <Routes>
-        <Route path="/" element={<Inicio />} />
-        <Route path="/sobremim" element={<Sobremim />} />
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<Sobremim />} />
+        </Route>
+
         <Route path="*" element={<div>Pagina não encontrada</div>} />
       </Routes>
 
