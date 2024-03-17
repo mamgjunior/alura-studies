6364c48a5194c85895684c27105a658e874bb081
Marcos Antonio Maia Gomes Junior <marcos.gomes@underprotection.com.br>
2024-03-17 20:25:40 -0300

    Criando teste no formu´lario

---
+++ bytebank/src/__test__/componentes/transacao/formulario/formulario.spec.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { render, screen, act } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Formulario from "../../../../componentes/Transacao/Formulario";
+
+// - A função act() é usada para garantir que as atualizações de estado do React sejam
+// tratadas de forma síncrona durante os testes.
+// - Ao usar act(), você está envolvendo a função render() e userEvent.type() em um contexto
+// onde as atualizações de estado do React são tratadas corretamente durante os testes.
+
+describe("Must render an input field", () => {
+  it("In the document", () => {
+    act(() => {
+      render(<Formulario />);
+    });
+    const campoTexto = screen.getByPlaceholderText("Digite um valor");
+    expect(campoTexto).toBeInTheDocument();
+  });
+
+  it("With type number", () => {
+    act(() => {
+      render(<Formulario />);
+    });
+    const campoTexto = screen.getByPlaceholderText("Digite um valor");
+    expect(campoTexto).toHaveAttribute("type", "number");
+  });
+
+  it("Which can be filled", () => {
+    act(() => {
+      render(<Formulario />);
+    });
+    const campoTexto = screen.getByPlaceholderText("Digite um valor");
+    act(() => {
+      userEvent.type(campoTexto, "50");
+    });
+    expect(campoTexto).toHaveValue(50);
+  });
+});
