b7be04e638488f7335df5b88f4b986c2b62cdc42
Marcos Antonio Maia Gomes Junior <marcos.gomes@underprotection.com.br>
2024-03-19 16:22:33 -0300

    Controlando rotas no teste

--- bytebank-v2/src/__test__/routes.test.js
+++ bytebank-v2/src/__test__/routes.test.js
@@ -1,6 +1,7 @@
 import { act, render, screen } from '@testing-library/react';
 import App from '../paginas/Principal/App';
-import { BrowserRouter } from 'react-router-dom';
+import Cartoes from '../componentes/Cartoes';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 
 describe('Rotas', () => {
   test('Deve renderizar a rota principal', () => {
@@ -10,5 +11,23 @@ describe('Rotas', () => {
 
     const usuario = screen.getByText('Olá, Joana :)!');
     expect(usuario).toBeInTheDocument();
+  });
+
+  test('Deve renderizar a rota Cartões', () => {
+    const rota = '/cartoes';
+    act(() => {
+      render(
+        <MemoryRouter initialEntries={[rota]}>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route path="cartoes" element={<Cartoes />} />
+            </Route>
+          </Routes>
+        </MemoryRouter>
+      );
+    });
+
+    const meusCartoes = screen.getByText('Meus cartões');
+    expect(meusCartoes).toHaveTextContent('Meus cartões');
   });
 });
